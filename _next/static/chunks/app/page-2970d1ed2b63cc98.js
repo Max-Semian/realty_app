b91ce1460ed0032a4c6e7e8ede6416e0d9f1c4ca
Max-Semian <Max-Semian@users.noreply.github.com>
2025-03-17 14:46:09 +0000

    deploy: 9cc4d3751fe253c81605b1dc885c8d02e68f1363

---
+++ _next/static/chunks/app/page-2970d1ed2b63cc98.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{2349:function(n,e,_){Promise.resolve().then(_.t.bind(_,345,23)),Promise.resolve().then(_.t.bind(_,2474,23))},345:function(n){n.exports={page:"page_page__ZU32B",main:"page_main__GlU4n"}},2474:function(){}},function(n){n.O(0,[971,69,744],function(){return n(n.s=2349)}),_N_E=n.O()}]);
