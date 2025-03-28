67a42446da71d650682aa1f93be93dcd33c237d8
Max-Semian <maxsemian@gmail.com>
2025-03-28 17:15:06 +0700

    layout fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" style={{height: '100%'}}>
-      <body style={{height: '100%'}}>{children}</body>
+      <body style={{height: '100%'}}>
+        {/* Fixed background element */}
+        <div 
+          style={{
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            backgroundImage: 'url(/realty_app/bg1.jpg)',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+            zIndex: -1,
+            pointerEvents: 'none'
+          }}
+        />
+        {children}
+      </body>
     </html>
   );
 }
