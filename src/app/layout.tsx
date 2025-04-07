219a0bc713498d42334ca81e7219306fc6994161
Leo G <forhotchpotch@gmail.com>
2025-04-07 07:33:40 +0000

    Remove number detection

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Add this meta tag to improve mobile rendering */}
+        <meta name="format-detection" content="telephone=no"></meta>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
       </head>
       <body style={{
